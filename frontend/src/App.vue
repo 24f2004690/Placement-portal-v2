0c559a3bb8d953aa7b731edd6f23c80fe6ff936f
unknown <24F2004690@ds.study.iitm.ac.in>
2026-07-15 15:39:44 +0530

    default app.vue page

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useRoute } from 'vue-router'
+// REMOVED NavBar import
+import { RouterView } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<template>
+  <div class="d-flex flex-column min-vh-100">
+    
+    <main class="flex-grow-1">
+      <RouterView />
+    </main>
+    
+    <AppFooter v-if="!route.meta.hideNavbar" />
+    
+  </div>
+</template>
+
+<style>
+/* Global styles remain */
+</style>

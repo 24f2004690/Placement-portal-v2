8ecccdee666e0bd98f32e96fa040a7bd650e0e98
kurtz <24f2004690@ds.study.iitm.ac.in>
2026-09-24 12:40:02 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -52,9 +52,9 @@ app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'tiblue1729@gmail.com'
-app.config['MAIL_PASSWORD'] = 'mdmcaqsbmxmpqbby'
-app.config['MAIL_DEFAULT_SENDER'] = 'tiblue1729@gmail.com'
+app.config['MAIL_USERNAME'] = 'yourmail@domain.com'
+app.config['MAIL_PASSWORD'] = '*******************'
+app.config['MAIL_DEFAULT_SENDER'] = 'yourmail@domain.com'
 
 mail = Mail(app)
 
